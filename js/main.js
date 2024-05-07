ca35897771c619a9cfd3f993fb2ccc57f4c0b206
Adelina091 <adelina.kabirova19@gmail.com>
2024-05-07 11:17:52 +0500

    Инициализация переменных для настроек игры и состояния

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('#game'); // Получаем элемент canvas
+
+// Получаем элементы для отображения статистики игры
+const mappingFoot = document.querySelector('.header__foot'); // Длина
+const mappingRecord = document.querySelector('.header__record'); // Рекорд
+const mappingAttemps = document.querySelector('.header__attemps'); // Попытки
