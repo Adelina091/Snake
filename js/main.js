88691ebb81a4762d2557558225d068d0dc5aa716
Adelina091 <adelina.kabirova19@gmail.com>
2024-05-07 11:19:37 +0500

    Функция для изменения скорости змеи

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ const canvas = document.querySelector('#game'); // Получаем элемен
 const mappingFoot = document.querySelector('.header__foot'); // Длина
 const mappingRecord = document.querySelector('.header__record'); // Рекорд
 const mappingAttemps = document.querySelector('.header__attemps'); // Попытки
+
+if (canvas.getContext) { // Проверяем поддержку canvas в браузере
+    const ctx   = canvas.getContext('2d'); // Получаем 2D контекст рисования
+
+    // Инициализация переменных для настроек игры и состояния
+
+    let mapWidth = canvas.width; // Ширина canvas
+    let mapHeight = canvas.height; // Высота canvas
+    let defaultSpeed = 5; // Скорость по умолчанию для змеи
+    let speed = 5; // Текущая скорость змеи
+    let interval = 1000 / speed; // Время между кадрами
+    let then = performance.now(); // Время последнего кадра
+
+
+}
